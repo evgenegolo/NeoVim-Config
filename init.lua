80c23fe41bc5287e84f320b1e68d427b074b0c95
evgenyg <evgenyg@tagvs.com>
2024-03-12 16:44:39 +0200

    more changes

--- init.lua
+++ init.lua
@@ -149,26 +149,6 @@ require('lazy').setup({
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
-  {
-    -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
-    build = ':TSUpdate',
-  },
-  -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
-  --       These are some example plugins that I've included in the kickstart repository.
-  --       Uncomment any of the lines below to enable them.
-  -- require 'kickstart.plugins.autoformat',
-  -- require 'kickstart.plugins.debug',
-
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
-  --    up-to-date with whatever is in the kickstart repo.
-  --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --
-  --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
   { import = 'plugins' },
 }, {})
 
@@ -469,7 +449,7 @@ cmp.setup {
   },
   sources = {
     -- Copilot Source
-    { name = "copilot",  group_index = 2 },
+    --[[  { name = "copilot",  group_index = 2 }, ]]
     -- Other Sources
     { name = "nvim_lsp", group_index = 2 },
     { name = "path",     group_index = 2 },
@@ -478,7 +458,7 @@ cmp.setup {
   sorting = {
     priority_weight = 2,
     comparators = {
-      require("copilot_cmp.comparators").prioritize,
+      --[[   require("copilot_cmp.comparators").prioritize, ]]
 
       -- Below is the default comparitor list and order for nvim-cmp
       cmp.config.compare.offset,
